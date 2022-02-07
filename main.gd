627a26a6f58d4de4096f3bb6425e22e25410f2cc
troido <troido@protonmail.com>
2022-02-07 21:08:44 +0100

    initial commit

---
+++ main.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+func _input(event):
+	# Mouse in viewport coordinates.
+	if event is InputEventMouseButton:
+		print("Mouse Click/Unclick at: ", event.position)
+	elif event is InputEventMouseMotion:
+		print("Mouse Motion at: ", event.position)
+
+	# Print the size of the viewport.
+	print("Viewport Resolution is: ", get_viewport_rect().size)
